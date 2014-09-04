6768a3800935a88285801983be09589906cd3f73
Andrea Vacondio <andrea.vacondio@gmail.com>
2014-09-04 17:54:48 +0200

    Unit tests

--- pdfsam-gui/src/test/java/org/pdfsam/ui/dashboard/DashboardTest.java
+++ pdfsam-gui/src/test/java/org/pdfsam/ui/dashboard/DashboardTest.java
@@ -1,6 +1,6 @@
 /* 
  * This file is part of the PDF Split And Merge source code
- * Created on 28/ago/2014
+ * Created on 03/set/2014
  * Copyright 2013-2014 by Andrea Vacondio (andrea.vacondio@gmail.com).
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,10 +16,10 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.pdfsam.ui.workarea;
+package org.pdfsam.ui.dashboard;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -28,8 +28,7 @@ import static org.sejda.eventstudio.StaticStudio.eventStudio;
 import java.util.Arrays;
 import java.util.List;
 
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 import javax.inject.Inject;
 
@@ -37,12 +36,10 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.pdfsam.module.Module;
-import org.pdfsam.module.UsageService;
 import org.pdfsam.test.ClearEventStudioRule;
-import org.pdfsam.test.DefaultPriorityTestModule;
 import org.pdfsam.test.InitializeAndApplyJavaFxThreadRule;
-import org.pdfsam.ui.event.SetActiveModuleRequest;
+import org.pdfsam.ui.dashboard.about.AboutDashboardPane;
+import org.pdfsam.ui.event.SetActiveDashboardItemRequest;
 import org.pdfsam.ui.event.SetTitleEvent;
 import org.sejda.eventstudio.Listener;
 import org.springframework.context.ApplicationContext;
@@ -58,7 +55,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-public class WorkAreaTest {
+public class DashboardTest {
     @ClassRule
     public static ClearEventStudioRule STUDIO_RULE = new ClearEventStudioRule();
     @Rule
@@ -70,53 +67,49 @@ public class WorkAreaTest {
     @Lazy
     static class Config {
         @Bean
-        public List<Module> modules() {
-            return Arrays.asList(new DefaultPriorityTestModule() {
-                @Override
-                public Pane modulePanel() {
-                    HBox panel = new HBox();
-                    panel.setId("modulePane");
-                    return panel;
-                }
-            });
+        public AboutDashboardPane aboutPane() {
+            AboutDashboardPane about = new AboutDashboardPane("PDFsam", "version");
+            about.setId("aboutPane");
+            return about;
         }
 
         @Bean
-        public UsageService service() {
-            return mock(UsageService.class);
+        public AboutDashboadItem item() {
+            return new AboutDashboadItem(aboutPane());
         }
 
         @Bean
-        public QuickbarModuleButtonsProvider provider() {
-            return new QuickbarModuleButtonsProvider(service(), modules());
+        public List<DashboardItem> items() {
+            return Arrays.asList(item());
         }
 
         @Bean
-        public QuickbarModuleButtonsPane buttons() {
-            return new QuickbarModuleButtonsPane(provider());
+        public QuickbarDashboardButtonsPane buttons() {
+            return new QuickbarDashboardButtonsPane(items());
         }
 
         @Bean
-        public WorkArea victim() {
-            return new WorkArea(modules(), buttons());
+        public Dashboard victim() {
+            return new Dashboard(items(), buttons());
         }
     }
 
     @Test
     public void wrongModuleDoesntBoom() {
-        WorkArea victim = applicationContext.getBean(WorkArea.class);
-        victim.onSetActiveModule(SetActiveModuleRequest.activeteModule("chuck norris"));
+        Dashboard victim = applicationContext.getBean(Dashboard.class);
+        victim.onSetActiveDashboardItem(new SetActiveDashboardItemRequest("chuck norris"));
     }
 
     @Test
     public void eventIsSent() {
-        WorkArea victim = applicationContext.getBean(WorkArea.class);
-        assertNull(victim.lookup("#modulePane"));
+        Dashboard victim = applicationContext.getBean(Dashboard.class);
+        assertTrue(((StackPane) victim.getCenter()).getChildren().isEmpty());
         Listener<SetTitleEvent> listener = mock(Listener.class);
         eventStudio().add(SetTitleEvent.class, listener);
-        victim.onSetActiveModule(SetActiveModuleRequest.activeteModule(DefaultPriorityTestModule.ID));
+        victim.onSetActiveDashboardItem(new SetActiveDashboardItemRequest(applicationContext.getBean(
+                AboutDashboadItem.class).id()));
         verify(listener).onEvent(any());
-        assertNotNull(victim.lookup("#modulePane"));
+        assertFalse(((StackPane) victim.getCenter()).getChildren().isEmpty());
     }
 
 }
