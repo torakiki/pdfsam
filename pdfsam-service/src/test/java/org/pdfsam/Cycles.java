95589457116d19c904b3aeb410ddbb4a82ebe7fa
Andrea Vacondio <andrea.vacondio@gmail.com>
2014-09-10 10:43:51 +0200

    Refactored cycles tests

--- pdfsam-service/src/test/java/org/pdfsam/Cycles.java
+++ pdfsam-service/src/test/java/org/pdfsam/Cycles.java
@@ -18,18 +18,6 @@
  */
 package org.pdfsam;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-
-import jdepend.framework.JDepend;
-import jdepend.framework.JavaPackage;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Unit test to test against cycles.
@@ -37,25 +25,6 @@ import org.slf4j.LoggerFactory;
  * @author Andrea Vacondio
  * @see http://www.softwarepoets.org/2009/04/unit-tests-to-check-against-cyclic.html
  */
-public class TestCycles {
-
-    private static final Logger LOG = LoggerFactory.getLogger(TestCycles.class);
-
-    private JDepend jdepend = new JDepend();
-    private Collection<? extends Object> packages = new ArrayList<>();
-
-    @Before
-    public void setUp() throws IOException {
-        jdepend.addDirectory("target/classes");
-        packages = jdepend.analyze();
-    }
-
-    @Test
-    public void cycleTest() {
-        for (Object p : packages) {
-            JavaPackage pack1 = (JavaPackage) p;
-            Assert.assertFalse(pack1.getName() + " failed.", pack1.containsCycle());
-            LOG.debug(String.format("%s analyzed for cycles.", pack1.getName()));
-        }
-    }
+public class Cycles extends TestCycles {
+    // nothing
 }
