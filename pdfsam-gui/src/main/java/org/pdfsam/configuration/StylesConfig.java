8f34cac89564103d659588411e5a24172491afe8
Andrea Vacondio <andrea.vacondio@gmail.com>
2015-09-28 10:26:16 +0200

    close #93: workaround for transparet windows incapable platforms

--- pdfsam-gui/src/main/java/org/pdfsam/configuration/StylesConfig.java
+++ pdfsam-gui/src/main/java/org/pdfsam/configuration/StylesConfig.java
@@ -30,6 +30,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import de.jensd.fx.glyphs.GlyphsStyle;
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
 
 /**
  * Styles configuration for the application
@@ -47,6 +49,10 @@ public class StylesConfig {
         LOG.debug(DefaultI18nContext.getInstance().i18n("Installing theme {0}.", theme.friendlyName()));
         theme.styleSheets().stream().map(s -> this.getClass().getResource(s).toExternalForm()).forEach(styles::add);
         styles.add(this.getClass().getResource(GlyphsStyle.DEFAULT.getStylePath()).toExternalForm());
+        if (!Platform.isSupported(ConditionalFeature.TRANSPARENT_WINDOW)) {
+            styles.add(this.getClass().getResource("/themes/transparent-incapable.css").toExternalForm());
+            LOG.info("Transparent windows not supported by the platform");
+        }
     }
 
     /**
