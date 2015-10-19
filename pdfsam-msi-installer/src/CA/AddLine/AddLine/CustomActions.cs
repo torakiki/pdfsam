cc07dda9b2729dc0b3a9ba0c9f5def0ec488f5ed
Andrea Vacondio <andrea.vacondio@gmail.com>
2015-10-19 19:38:10 +0200

    ref #45, #50: added custom action to write properties to the l4j ini file

---
+++ pdfsam-msi-installer/src/CA/AddLine/AddLine/CustomActions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+using System.IO;
+
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult AddLine(Session session)
+        {
+          try{
+                string path = session.CustomActionData["folder"] + "pdfsam.l4j.ini";                
+                if (File.Exists(@path)) 
+                {
+                    session.Log("AddLine to existing " + session.CustomActionData["folder"] + "pdfsam.l4j.ini");
+                    File.AppendAllText(path, Environment.NewLine + "-Dorg.pdfsam.default.checkforupdate=" + session.CustomActionData["updates"] + Environment.NewLine);
+                    session.Log("Added -Dorg.pdfsam.default.checkforupdate=" + session.CustomActionData["updates"]);
+                    File.AppendAllText(path, "-Dorg.pdfsam.default.locale=" + session.CustomActionData["locale"] + Environment.NewLine);
+                    session.Log("Added -Dorg.pdfsam.default.locale=" + session.CustomActionData["locale"]);                     
+                }else   
+                {
+                    session.Log("Uable to find l4j ini file");
+                }
+	      }catch (Exception){
+		        return ActionResult.Failure;
+	      }
+	      return ActionResult.Success;
+        }
+    }
+
