1913177e14c81c1fbcab745617944f02405ff0db
Andrea Vacondio <andrea.vacondio@gmail.com>
2015-10-28 17:11:35 +0100

    close #106: not exactly fixed but it's now a bit better

--- pdfsam-msi-installer/src/Build.cmd
+++ pdfsam-msi-installer/src/Build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-SET MsiName=pdfsam-v3.0.0.M3
+SET PDFSAM_VERSION=3.0.0.M3
 
 REM Prevent compiling with outdated pdfsam.wixobj file if there is a error in candle.
 del /Q pdfsam.wixobj
@@ -13,7 +13,7 @@ REM Build the MSI
 "%WIX%bin\candle.exe" pdfsam.wxs requirementsDlg.wxs featuresTree.wxs verifyWithLanguageDlg.wxs exitDlg.wxs -ext WixUIExtension -ext WixUtilExtension
 
 REM English
-IF EXIST pdfsam.wixobj "%WIX%bin\light.exe" pdfsam.wixobj requirementsDlg.wixobj verifyWithLanguageDlg.wixobj featuresTree.wixobj exitDlg.wixobj -ext WixUIExtension -ext WixUtilExtension -spdb -out "output\%MsiName%.msi" -loc "culture.wxl" -cultures:en-us
+IF EXIST pdfsam.wixobj "%WIX%bin\light.exe" pdfsam.wixobj requirementsDlg.wixobj verifyWithLanguageDlg.wixobj featuresTree.wixobj exitDlg.wixobj -ext WixUIExtension -ext WixUtilExtension -spdb -out "output\pdfsam-v%PDFSAM_VERSION%.msi" -loc "culture.wxl" -cultures:en-us
 
 REM Cleanup
 del /Q pdfsam.wixobj
