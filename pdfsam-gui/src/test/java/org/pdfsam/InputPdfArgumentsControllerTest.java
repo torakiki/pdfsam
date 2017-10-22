63cae44de2539b83ba501dd7d19b0b92eafc9551
torakiki <andrea.vacondio@gmail.com>
2017-10-22 12:03:54 +0200

    Copyright notice, hopefully we got it right this time

--- pdfsam-gui/src/test/java/org/pdfsam/InputPdfArgumentsControllerTest.java
+++ pdfsam-gui/src/test/java/org/pdfsam/InputPdfArgumentsControllerTest.java
@@ -1,7 +1,7 @@
 /* 
  * This file is part of the PDF Split And Merge source code
  * Created on 09 ago 2016
- * Copyright 2017 by Sober Lemur S.a.s. di Andrea Vacondio (info@pdfsam.org).
+ * Copyright 2017 by Sober Lemur S.a.s. di Vacondio Andrea (info@pdfsam.org).
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as 
