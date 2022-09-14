dd7d50538c0c2c81ada5d0047d0bdb504d728eaa
torakiki <andrea.vacondio@gmail.com>
2022-09-14 15:56:47 +0200

    Added module info to i18n module

---
+++ pdfsam-i18n/src/main/java/module-info.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the PDF Split And Merge source code
+ * Created on 14/09/22
+ * Copyright 2022 by Sober Lemur S.a.s. di Vacondio Andrea (info@pdfsam.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module org.pdfsam.i18n {
+    exports org.pdfsam.i18n;
+
+    requires org.pdfsam.eventstudio;
+    requires org.sejda.commons;
+    requires transitive io.reactivex.rxjava3;
+    requires org.slf4j;
+}
