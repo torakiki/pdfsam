5b26660b38b45959006cbd8bfe097db6ef29afa6
torakiki <andrea.vacondio@gmail.com>
2023-02-02 18:48:33 +0100

    Custom action used in msi installer to cope with the new package structure

--- pdfsam-basic/src/msi/CA/AddLine/AddLine/CustomAction.cs
+++ pdfsam-basic/src/msi/CA/AddLine/AddLine/CustomAction.cs
@@ -10,29 +10,29 @@ using System.IO;
         public static ActionResult AddLine(Session session)
         {
           try{
-                string path = session.CustomActionData["folder"] + "pdfsam.l4j.ini";                
+                string path = session.CustomActionData["folder"] + "app/pdfsam.cfg";                
                 if (File.Exists(@path)) 
                 {
-                    session.Log("AddLine to existing " + session.CustomActionData["folder"] + "pdfsam.l4j.ini");
-                    File.AppendAllText(path, Environment.NewLine + "-Dorg.pdfsam.default.checkforupdate=" + session.CustomActionData["updates"] + Environment.NewLine);
+                    session.Log("AddLine to existing " + session.CustomActionData["folder"] + "app/pdfsam.cfg");
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.checkforupdate=" + session.CustomActionData["updates"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.checkforupdate=" + session.CustomActionData["updates"]);
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.donate.notification=" + session.CustomActionData["donate"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.donate.notification=" + session.CustomActionData["donate"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.donate.notification=" + session.CustomActionData["donate"]);
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.locale=" + session.CustomActionData["locale"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.locale=" + session.CustomActionData["locale"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.locale=" + session.CustomActionData["locale"]);                     
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.checkfornews=" + session.CustomActionData["news"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.checkfornews=" + session.CustomActionData["news"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.checkfornews=" + session.CustomActionData["news"]);                     
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.play.sounds=" + session.CustomActionData["sounds"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.play.sounds=" + session.CustomActionData["sounds"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.play.sounds=" + session.CustomActionData["sounds"]);                     
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.fetch.premium.modules=" + session.CustomActionData["premium"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.fetch.premium.modules=" + session.CustomActionData["premium"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.fetch.premium.modules=" + session.CustomActionData["premium"]);
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.compression=" + session.CustomActionData["compression"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.compression=" + session.CustomActionData["compression"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.compression=" + session.CustomActionData["compression"]);
-                    File.AppendAllText(path, "-Dorg.pdfsam.default.output.overwrite=" + session.CustomActionData["overwrite"] + Environment.NewLine);
+                    File.AppendAllText(path, "java-options=-Dorg.pdfsam.default.output.overwrite=" + session.CustomActionData["overwrite"] + Environment.NewLine);
                     session.Log("Added -Dorg.pdfsam.default.output.overwrite=" + session.CustomActionData["overwrite"]);                      
                 }else   
                 {
-                    session.Log("Unable to find l4j ini file");
+                    session.Log("Unable to find config file");
                 }
 	      }catch (Exception){
 		        return ActionResult.Failure;
