5ffec4c2f71939efb2d674f851de8cce319c5187
torakiki <andrea.vacondio@gmail.com>
2024-07-29 10:18:13 +0200

    removed log statements. Added a script to sign the dll file

---
+++ pdfsam-basic/src/msi/SignDll.cmd
@@ -0,0 +1,11 @@
+ECHO OFF
+
+IF NOT EXIST "AddLine.CA.dll" GOTO error
+attrib -r "AddLine.dll"
+"signtool.exe" sign /v /debug /fd sha256 /td sha256 /tr http://sha256timestamp.ws.symantec.com/sha256/timestamp /sha1 "%SIGN_CERT_FINGERPRINT%" "AddLine.dll"
+if %ERRORLEVEL% NEQ 0 goto error
+ECHO "Dll signed"
+
+:error
+set ERROR_CODE=%ERRORLEVEL%
+exit /B %ERROR_CODE%
