21aecb52df691a5b584ea802f63bdaafb7cf0cb5
Junming Chen <25316415+Chapoly1305@users.noreply.github.com>
2025-11-13 13:25:08 -0500

    Remove confirmation of creating new directory

--- pdfsam-ui-components/src/main/java/org/pdfsam/ui/components/io/BrowsableOutputDirectoryField.java
+++ pdfsam-ui-components/src/main/java/org/pdfsam/ui/components/io/BrowsableOutputDirectoryField.java
@@ -18,6 +18,7 @@
  */
 package org.pdfsam.ui.components.io;
 
+import javafx.scene.control.Label;
 import org.pdfsam.core.context.ApplicationContext;
 import org.pdfsam.core.context.BooleanPersistentProperty;
 import org.pdfsam.core.support.params.MultipleOutputTaskParametersBuilder;
@@ -25,17 +26,21 @@ import org.pdfsam.core.support.params.TaskParametersBuildStep;
 import org.pdfsam.eventstudio.annotation.EventListener;
 import org.pdfsam.eventstudio.annotation.EventStation;
 import org.pdfsam.model.tool.ToolBound;
-import org.pdfsam.model.ui.NonExistingOutputDirectoryEvent;
 import org.pdfsam.model.ui.SetOutputDirectoryRequest;
 import org.pdfsam.ui.components.support.FXValidationSupport;
+import org.pdfsam.ui.components.support.Style;
 import org.sejda.model.parameter.base.SingleOrMultipleOutputTaskParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.function.Consumer;
 
 import static org.apache.commons.lang3.StringUtils.defaultString;
 import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 import static org.pdfsam.core.context.ApplicationContext.app;
 import static org.pdfsam.core.context.StringPersistentProperty.WORKING_PATH;
 import static org.pdfsam.core.support.validation.Validators.and;
@@ -52,7 +57,9 @@ import static org.sejda.model.output.FileOrDirectoryTaskOutput.directory;
 public class BrowsableOutputDirectoryField extends BrowsableDirectoryField
         implements TaskParametersBuildStep<MultipleOutputTaskParametersBuilder<?>>, ToolBound {
 
+    private static final Logger LOG = LoggerFactory.getLogger(BrowsableOutputDirectoryField.class);
     private String ownerModule = "";
+    private final Label infoLabel = new Label();
 
     public BrowsableOutputDirectoryField() {
         this(app(), "");
@@ -72,6 +79,33 @@ public class BrowsableOutputDirectoryField extends BrowsableDirectoryField
         eventStudio().addAnnotatedListeners(this);
         context.persistentSettings().get(WORKING_PATH).ifPresent(getTextField()::setText);
         getTextField().setValidator(and(nonBlank(), v -> !Files.isRegularFile(Paths.get(v))));
+
+        // Setup info label
+        infoLabel.getStyleClass().addAll("info-message");
+        infoLabel.setStyle("-fx-text-fill: #2E7D32; -fx-padding: 2 0 0 2;");
+        infoLabel.setManaged(false);
+        infoLabel.setVisible(false);
+
+        // Add listener to show/hide info message
+        getTextField().textProperty().addListener((obs, oldVal, newVal) -> updateInfoLabel());
+    }
+
+    private void updateInfoLabel() {
+        if (isNotBlank(getTextField().getText())) {
+            var path = Paths.get(getTextField().getText());
+            if (!Files.exists(path)) {
+                infoLabel.setText(i18n().tr("Folder will be created"));
+                infoLabel.setManaged(true);
+                infoLabel.setVisible(true);
+                return;
+            }
+        }
+        infoLabel.setManaged(false);
+        infoLabel.setVisible(false);
+    }
+
+    public Label getInfoLabel() {
+        return infoLabel;
     }
 
     @EventListener
@@ -79,6 +113,7 @@ public class BrowsableOutputDirectoryField extends BrowsableDirectoryField
         if (!event.fallback() || isBlank(getTextField().getText()) || app().persistentSettings()
                 .get(BooleanPersistentProperty.SMART_OUTPUT)) {
             getTextField().setText(event.directory().getAbsolutePath());
+            updateInfoLabel();
         }
     }
 
@@ -94,9 +129,19 @@ public class BrowsableOutputDirectoryField extends BrowsableDirectoryField
         getTextField().validate();
         if (getTextField().getValidationState() == FXValidationSupport.ValidationState.VALID) {
             var output = Paths.get(getTextField().getText());
+
+            // Auto-create directory if it doesn't exist
             if (!Files.exists(output)) {
-                eventStudio().broadcast(new NonExistingOutputDirectoryEvent(output));
+                try {
+                    Files.createDirectories(output);
+                    LOG.debug("Created output directory {}", output);
+                } catch (IOException e) {
+                    LOG.warn("Unable to create output directory", e);
+                    onError.accept(i18n().tr("Unable to create output directory: {0}", e.getMessage()));
+                    return;
+                }
             }
+
             if (Files.isDirectory(output)) {
                 builder.output(directory(output.toFile()));
             } else {
